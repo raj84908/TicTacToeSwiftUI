29c33e707c362947445b67d17674c179e5ae13f5
Raj Patel <raj84908@gmail.com>
2024-03-22 11:04:48 -0400

    Initial Commit

---
+++ TicTacToeSwiftUI/TicTacToeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIApp.swift
+//  TicTacToeSwiftUI
+//
+//  Created by Raj Patel on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
